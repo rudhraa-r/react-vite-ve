8f7a21d3f9c4a0d0bded2ad30ce9c1fed5f01057
rudhraa_r <rudhraa2004@gmail.com>
2024-06-23 01:27:47 +0530

    photo by link

--- api/index.js
+++ api/index.js
@@ -14,7 +14,8 @@ const bcryptSalt = bcrypt.genSaltSync(10);
 const jwtSecret = 'skdgnoerig';
 
 app.use(express.json());
-app.use(cookieParser());
+app.use(cookieParser()) ;
+app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cors({
     credentials: true,
     origin:'http://localhost:5173',
@@ -129,12 +130,15 @@ app.get('/create', (req, res) =>{
 
 });
 
+ 
 app.post('/upload-by-link', async (req, res) =>{
     const {link} = req.body;
+    const newName = 'photo'+ Date.now() + '.jpg';
     await imageDownloader.image({
         url: link,
-        dest: __dirname + '/uploads'
-    })
-})
+        dest: __dirname + '/uploads/'+ newName ,
+    });
+    res.json(newName);
+} )
 
 app.listen(4000);
