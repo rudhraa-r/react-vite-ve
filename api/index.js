170b8f82097b8ed90cc49f4aed774bd65a33c78c
rudhraa_r <rudhraa2004@gmail.com>
2024-07-26 15:58:12 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ app.use(cookieParser()) ;
 app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cors({
     credentials: true,
-    origin:'https://react-vite-ve-git-main-rudhraas-projects.vercel.app',
+    origin:' http://localhost:5173/',
 }));
 
 
